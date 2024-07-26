98290259ebf80e6b54f142652ab9c2930fa7169a
Workshop <dummy_workshop_email@example.com>
2024-07-26 15:16:55 +0100

    Button that exists

--- static/css/js/changeColour.js
+++ static/css/js/changeColour.js
@@ -1,4 +1,5 @@
 document.getElementById("colorButton").addEventListener("click", function() {
     const randomColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
-    document.body.style.backgroundColor = randomColor;
+    const paddedColor = randomColor.padEnd(7, '0');
+    document.body.style.backgroundColor = paddedColor;
 })
