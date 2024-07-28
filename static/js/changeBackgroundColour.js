e075d022883d6e55dee7a567aef86d9433e04196
Zanete Dijeva <zanete.dijeva@gmail.com>
2024-07-28 19:23:51 +0100

    Button that changes background

--- static/js/changeBackgroundColour.js
+++ static/js/changeBackgroundColour.js
@@ -1,5 +1,6 @@
-document.getElementById("colorButton").addEventListener("click", function() {
+function changeBackgroundColour() {
+    document.getElementById("mainBody");
     const randomColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
     const paddedColor = randomColor.padEnd(7, '0');
     document.body.style.backgroundColor = paddedColor;
-})
+}
